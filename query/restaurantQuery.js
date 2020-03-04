a04d4bdae87dada398a31eb06f8b555fb762c2e8
hoyinho <hoyinho999@gmail.com>
2020-03-04 14:21:44 -0500

    added skeleton file for restaurant queries

---
+++ query/restaurantQuery.js
@@ -0,0 +1,14 @@
+//Input: String id
+//Return: JSON containing all information of a restaurant using a restaurant ID
+function getAllInfoFromID(id){
+}
+
+//Input: String cuisine
+//Return: Array of all restaurant IDs that contain the given cuisine type (Would eventually want to have this return restaurants that have a cuisine type similar to the input. IE input sushi and returns japanese restaurants. Also would hope to have error matching. IE japnese returns japanese restaurants.
+function getAllIDFromCuisine(cuisine){
+}
+
+//Input: array of Strings ids
+//Returns: Array of Json of information pertaining to each restaurant ID given. Only returns the first image link, address, hours, rating, and name
+function getReducedInfoFromIDs(ids){
+}
